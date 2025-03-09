45623b0e14aeaab271fddeb9530c8e0be9da805d
sone6ee <opo226@naver.com>
2025-03-10 03:34:32 +0900

    todo.js 수정

--- js/todo.js
+++ js/todo.js
@@ -12,16 +12,30 @@ function saveToDos() {
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
-    li.remove();
 
-    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
-    saveToDos();
+    li.style.opacity = "0";
+    li.style.transform = "translateY(10px)";
+    li.style.transition = "opacity 0.5s ease-out, transform 0.5s ease-out";
+
+    setTimeout(() => {
+        li.remove();
+        toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+        saveToDos();
+    }, 500);
 }
 
 function paintToDo(newTodoObj) {
     const li = document.createElement("li");
     li.id = newTodoObj.id;
     li.classList.add("todo-item");
+    li.style.opacity = "0";
+    li.style.transform = "translateY(10px)";
+    li.style.transition = "opacity 0.5s ease-out, transform 0.5s ease-out";
+
+    setTimeout(() => {
+        li.style.opacity = "1";
+        li.style.transform = "translateY(0)";
+    }, 10);
 
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
@@ -37,7 +51,7 @@ function paintToDo(newTodoObj) {
     const button = document.createElement("button");
     button.innerText = "Delete";
     button.classList.add("todo-delete");
-    button.addEventListener("click", deleteToDo);
+    button.addEventListener("click", (event) => deleteToDo(event, li));
 
     li.appendChild(checkbox);
     li.appendChild(span);
