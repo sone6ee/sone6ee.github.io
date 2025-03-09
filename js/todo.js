1d416a2a5a069906ff2f66895475f058925a45d2
sone6ee <opo226@naver.com>
2025-03-10 03:25:42 +0900

    To Do List 마무리 작업

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,46 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-function paintToDo(newTodo) {
+const TODOS_KEY = "todos";
+
+let toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
+}
+
+function paintToDo(newTodoObj) {
     const li = document.createElement("li");
+    li.id = newTodoObj.id;
+    li.classList.add("todo-item");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("todo-checkbox");
+    checkbox.addEventListener("change", () => {
+        span.classList.toggle("checked", checkbox.checked);
+    });
+
     const span = document.createElement("span");
+    span.innerText = newTodoObj.text;
+    span.classList.add("todo-text");
 
+    const button = document.createElement("button");
+    button.innerText = "Delete";
+    button.classList.add("todo-delete");
+    button.addEventListener("click", deleteToDo);
+
+    li.appendChild(checkbox);
     li.appendChild(span);
-    span.innerText = newTodo;
-
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
@@ -18,7 +51,23 @@ function handleToDoSubmit(event) {
     const newTodo = toDoInput.value;
     toDoInput.value = "";
 
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    }
+
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+
+    parsedToDos.forEach(paintToDo);
+}
